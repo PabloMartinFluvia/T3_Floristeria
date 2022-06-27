8861482bc2b67dbf4a804c6735718de7bcd03819
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-06-27 20:16:05 +0200

    added comment

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/App.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/App.java
@@ -1,5 +1,29 @@
 package com.calvogasullmartin.t3_floristeria;
 
 public class App {
-    private String a;
+    
+    public static void main(String[] args) {
+        App app = new App();
+        app.configurarInicializacion();
+        app.execute();
+    }
+    
+    /*
+    Inicializa la configuración inicial.
+    Que base de datos se usa, que familia de vistas, que famila de controladores, etc...
+    Como mínimo debería usar una vista, para saber al usuario que BD usar.
+    */
+    private void configurarInicializacion(){
+        //some code para inicializar las abstract facotry
+        
+        //es necesario inyectar a la DaoFactory una implementación concreta mediante
+        //su metodo estatico setFactory.
+    }
+    
+    /*
+    Arranca la aplicación
+    */
+    private void execute(){
+        //some code que permita ir canviando de controlador, según lo que le diga el usuario a las vistas
+    }
 }
