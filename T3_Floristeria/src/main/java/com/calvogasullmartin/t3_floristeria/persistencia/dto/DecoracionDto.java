5b81547dc3bbf7d1acea53c3641c8f3172afdafb
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-06-27 21:32:53 +0200

    paquete dto implementado

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/persistencia/dto/DecoracionDto.java
@@ -0,0 +1,16 @@
+package com.calvogasullmartin.t3_floristeria.persistencia.dto;
+
+import com.calvogasullmartin.t3_floristeria.modelos.Material;
+
+public class DecoracionDto {
+    
+    private Material material;
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }        
+}
