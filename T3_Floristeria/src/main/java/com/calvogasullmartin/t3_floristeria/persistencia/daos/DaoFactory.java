8eea96768f10023747cc6019292dd8c389643015
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-06-27 20:15:24 +0200

    paquete dao implementado
    Falta ir añadiendo metodos abstractos en funcion de operaciones CRUD particulares de cada dto

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/persistencia/daos/DaoFactory.java
@@ -0,0 +1,33 @@
+package com.calvogasullmartin.t3_floristeria.persistencia.daos;
+
+public abstract class DaoFactory {
+    
+    //public?
+    private static DaoFactory factory = null;
+
+    public static DaoFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public static void setFactory(DaoFactory factory) {
+        assert factory != null;
+        DaoFactory.factory = factory;
+    }
+    
+    public abstract boolean isBDInizializada();
+
+    public abstract void inicializarBD();        
+    
+    public abstract FloristeriaDao getFloristeriaDao();
+
+    public abstract StockDao getStockDao();
+
+    public abstract TicketDao getTicketDao();
+
+    public abstract ArbolDao getArbolDao();
+
+    public abstract FlorDao getFlorDao();
+    
+    public abstract DecoracionDao getDecoracionDao();
+}
---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/persistencia/daos/DecoracionDao.java
@@ -0,0 +1,8 @@
+package com.calvogasullmartin.t3_floristeria.persistencia.daos;
+
+import com.calvogasullmartin.t3_floristeria.persistencia.dto.DecoracionDto;
+
+public interface DecoracionDao extends GenericDao<DecoracionDto, Integer>{
+    
+    //any particular CRUD operations
+}
