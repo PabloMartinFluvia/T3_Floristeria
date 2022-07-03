752f86593c141d34c6f6236db51c7461f48317f8
MarcosManuelCalvo <101265172+MarcosManuelCalvo@users.noreply.github.com>
2022-07-03 14:01:07 +0300

    menuVistav2

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/menuVista.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/menuVista.java
@@ -17,4 +17,51 @@ public class menuVista {
         InOut io = new InOut();
         io.writeln(mensaje); 
     }
+
+    public static int seleccionaOpcion(int opciones){ 
+		int opcion;
+		InOut io = new InOut();
+		boolean ok = false;
+		do {
+			opcion = io.readInt("Selecciona Opcion: ");
+			if(opcion>=0 && opcion<=opciones){
+				ok = true;
+			}else{
+				io.writeln("Opcion no correcta. Debe ser entre 0 i "+opciones);
+			}            
+		}while(!ok);
+		return opcion;
+	}
+
+	public static void realizarTarea(Integer opcion, Floristeria floristeria){
+		switch(opcion) {
+		    case 1:
+                articuloVista(floristeria);
+                break;
+		    case 2:
+                retirarArticuloVista(floristeria);
+                break;
+		    case 3:
+                mostrarStockVista(floristeria);
+                break;
+		    case 4:
+                mostrarStockUnitVista(floristeria);
+                break;
+		    case 5:
+                valorFloristeriaVista(floristeria);
+                break;
+		    case 6:
+                ventaVista(floristeria);
+                break;
+		    case 7:
+                historialTicketsVista(floristeria);
+                break;
+		    case 8:
+                totalVentasVista(floristeria);
+                break;
+		    case 0:
+                //return
+			    break;
+		}        
+	}
 }
