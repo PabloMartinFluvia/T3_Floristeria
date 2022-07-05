476548df65102e9d41b12e57920ec35f2caf3c74
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-05 12:49:57 +0200

    done ConectorTxt.class

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/persistencia/txt/ConectorTxt.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/persistencia/txt/ConectorTxt.java
@@ -1,19 +1,30 @@
 package com.calvogasullmartin.t3_floristeria.persistencia.txt;
 
 import com.calvogasullmartin.t3_floristeria.persistencia.Conector;
+import java.io.File;
+import java.io.IOException;
 
 public class ConectorTxt implements Conector{
 
+    private final String rutaArchivoTxt;
+    private File archivoTxt;
+
+    public ConectorTxt() {
+        rutaArchivoTxt = "floristeria.txt"; // se crea en el mismo directorio que carpetas src y target
+        archivoTxt = new File(rutaArchivoTxt);        
+    }
+            
     @Override
-    public boolean isBDInicizializada() {
-        boolean ok = false;
-        //some code;
-        return ok;
+    public boolean isBDInicizializada() {       
+        return archivoTxt.exists() && !archivoTxt.isDirectory();
     }
 
     @Override
-    public void inicializadBD() {
-        //some code
-    }
-    
+    public void inicializarBD() throws IOException{
+        archivoTxt.createNewFile();
+    }    
+
+    public File getArchivoTxt() {
+        return archivoTxt;
+    }        
 }
