25ffad4fb388953587bdcf340029ef50903430ba
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-05 12:50:49 +0200

    removed comments

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/config/Configurador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/config/Configurador.java
@@ -9,27 +9,4 @@ public class Configurador{
     public void inyectarFactory() {        
         DaoFactory.setFactory(new DaoFactoryTxt());
     }
-    
-    //provisional, se guarda dentro de la carpeta src
-    //private final String rutaArchivoTxt = "floristeria.txt";
-    
-    /*
-    public boolean validarBD() {
-       File archivo = new File(rutaArchivoTxt);
-       if (archivo.exists() && !archivo.isDirectory()){
-           return true;
-       }else {
-           // mensaje de error: ruta del archivo no es valido
-           //mejor que pete el programa, o que canvie la ruta del archivo (y canviar el "final")
-           //etc... habrá que pensar un rato que hacer
-           return false;
-       }
-    }
-    */
-    
-    /*
-    public String getRutaArchivoTxt() {
-        return rutaArchivoTxt;
-    }  
-    */
 }
