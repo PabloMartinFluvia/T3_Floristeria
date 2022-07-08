7ad3145ae1709c759f82e4480bec9d44fd2e0dc4
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-08 17:23:38 +0200

    Update ProductoUnidad.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/modelos/ProductoUnidad.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/modelos/ProductoUnidad.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.calvogasullmartin.t3_floristeria.modelos;
 
-/**
- *
- * @author paumf
- */
 public class ProductoUnidad {
     
+    private ProductoCompleto producto;
+    
+    private Integer cantidad;
+
+    public ProductoUnidad() {
+        producto = null;
+        cantidad = null;
+    }
+
+    public ProductoCompleto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(ProductoCompleto producto) {
+        this.producto = producto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }        
 }
