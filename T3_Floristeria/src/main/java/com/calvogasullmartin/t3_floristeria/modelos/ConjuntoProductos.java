2ac3c8078c20cc96ca142ac82620d3d1d68d6b94
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-08 17:18:48 +0200

    Create ConjuntoProductos.java

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/modelos/ConjuntoProductos.java
@@ -0,0 +1,44 @@
+package com.calvogasullmartin.t3_floristeria.modelos;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ConjuntoProductos {
+    // es = stock = tiquet
+    private Integer id; // 1,2,3 para los stock de arboles, flores, decoracion, los siguientes para tiquets
+    
+    private Float valor_Productos; // sumatorio, en este conjunto, de: precio del producto * unidades 
+                                    // = valor de este stock = importe de este tiquet
+    private List<ProductoUnidad> productos; // lista de ProductoUnidad
+                                    //asociar producto y unidades de este se puede hacer un Map, pero para serializar hace falta esta clase intermedia
+
+    public ConjuntoProductos() {
+        id = null;
+        valor_Productos = null;
+        productos = new LinkedList<>();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Float getValor_Productos() {
+        return valor_Productos;
+    }
+
+    public void setValor_Productos(Float valor_Productos) {
+        this.valor_Productos = valor_Productos;
+    }
+
+    public List<ProductoUnidad> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<ProductoUnidad> productos) {
+        this.productos = productos;
+    }    
+}
