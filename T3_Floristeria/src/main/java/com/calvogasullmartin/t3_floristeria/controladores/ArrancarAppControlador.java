ff002e3b71e7b4b261a5a81668de47f05569d744
MarcosManuelCalvo <101265172+MarcosManuelCalvo@users.noreply.github.com>
2022-07-09 13:46:23 +0300

    Update ArrancarAppControlador.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ArrancarAppControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ArrancarAppControlador.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.calvogasullmartin.t3_floristeria.controladores;
 
 import com.calvogasullmartin.t3_floristeria.modelos.Estado;
 import com.calvogasullmartin.t3_floristeria.modelos.Floristeria;
 import java.io.IOException;
 
-/**
- *
- * @author paumf
- */
 public class ArrancarAppControlador extends ControladorFuncional{
     
     private Floristeria floristeria;
+    //private FloristeriaDao floristeriaDao;//sustituido
 
     public ArrancarAppControlador(Estado estado, Floristeria floristeria) {
         super(estado);
@@ -22,28 +15,37 @@ public class ArrancarAppControlador extends ControladorFuncional{
     }
 
     @Override
-    public void controlar() {
-        
+    public void controlar() {   
     }
-    
-    public boolean isPrimeraVez(){
-        return false;
-    }
-    
+
     public String getNombreFloristeria() throws IOException{
         return factory.getFloristeriaDao().findName();
     }
     
-    public void setEstado(Estado estado){
+    public void setEstado(Estado estado){//codigo repetido
         this.estado = estado;
     }
-    
-    public void guardarUnicaFloristeria (String nombre) throws IOException{
-        floristeria.setId(1);
-        floristeria.setNombre(nombre);
-        floristeria.setValorTotal(0);
-        factory.getFloristeriaDao().create(floristeria);
+
+    public getNombreFloristeria(){
+        return floristeriaDao.findName();
     }
-    
-    
+
+    public isPrimeraVez(){
+        Conector conector = DaoFactory.getFactory().getConector();
+        boolean t = true;
+        if(conector.isBDInicizializada()){
+            t=false;
+        }
+        else{
+            conector.inicializarBD();
+        }
+        return t;
+    }
+
+    public void guardarUnicaFloristeria (String nombre) throws IOException{
+        assert nombre != null;
+        floristeria.setFloristeria_id(1);
+        floristeria.setNombre_floristeria(nombre);
+        factory.getFloristeriaDao().create(floristeria);
+    }  
 }
