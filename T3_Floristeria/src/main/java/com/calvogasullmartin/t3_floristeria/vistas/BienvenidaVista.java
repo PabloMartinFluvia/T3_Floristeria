9e09349b886079a60c8f5d0490b2aaeb267d3a1a
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 15:00:11 +0200

    Update BienvenidaVista.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/BienvenidaVista.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/BienvenidaVista.java
@@ -4,28 +4,34 @@ import com.calvogasullmartin.t3_floristeria.controladores.ArrancarAppControlador
 import com.calvogasullmartin.t3_floristeria.utils.InOut;
 
 public class BienvenidaVista {
+
     private InOut io;
+    private ArrancarAppControladorInterface controlador;
 
     public BienvenidaVista() {
         io = new InOut();
     }
-    
+
     //solo interactua con un ArrncarAppControlador cualquiera
-    public void interactuar(ArrancarAppControladorInterface controlador){
+    public void interactuar(ArrancarAppControladorInterface controlador) {        
         assert controlador != null;
-        if (controlador.isPrimeraVez()){
-            controlador.iniciarPersistencia();
-            io.writeln("Capa de persistencia inizializada con exito");
-            new FloristeriaVista().interactuar(controlador);
-            
+        this.controlador = controlador; //lo guardamos en atributos para no tener que pasarselo a los métodos privados
+        if (controlador.isPrimeraVez()) {            
+            pedirInicializacion();
         }
-        String nombre = controlador.getNombreFloristeria();
-        darBienvienida(nombre);
-        controlador.seleccionarMenu();
+        darBienvienida();
+        controlador.seleccionarMenu(); //FIN DE LA FUNCIONALIDAD
     }
-    
-    private void darBienvienida(String nombre){    
-        assert nombre != null;        
-        io.writeln("Bienvenid@ al gestor de la floristeria "+nombre+", desarrollado por CalvoGasullMartin ");
+
+    private void pedirInicializacion() {
+        io.writeln("Aplicación ejecutandose por primera vez ...");
+        controlador.iniciarPersistencia();
+        io.writeln("Capa de persistencia inizializada con exito");
+        new FloristeriaVista().interactuar(controlador);
+    }
+
+    private void darBienvienida() {
+        String nombre = controlador.getNombreFloristeria();
+        io.writeln("Bienvenid@ al gestor de la floristeria " + nombre + ", desarrollado por CalvoGasullMartin ");
     }
 }
