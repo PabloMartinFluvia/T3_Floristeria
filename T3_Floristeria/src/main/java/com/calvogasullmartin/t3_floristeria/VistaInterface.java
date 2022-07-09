598a2732a00198e7b81b738c3bf405671d54a88b
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 11:36:25 +0200

    Create VistaInterface.java

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/VistaInterface.java
@@ -0,0 +1,9 @@
+package com.calvogasullmartin.t3_floristeria;
+
+import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncional;
+import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncionalVisitor;
+
+public interface VistaInterface extends ControladorFuncionalVisitor{
+    
+    public void interactua(ControladorFuncional controlador);
+}
