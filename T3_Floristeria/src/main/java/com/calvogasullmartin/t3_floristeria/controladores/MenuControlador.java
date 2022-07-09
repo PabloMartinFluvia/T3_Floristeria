f6671ace0ada1e90e9c36c57f16705df7a6d20a8
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 21:29:44 +0200

    Create MenuControlador.java

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/MenuControlador.java
@@ -0,0 +1,22 @@
+package com.calvogasullmartin.t3_floristeria.controladores;
+
+public interface MenuControlador extends ControladorFuncional{
+    
+    public void introducirArticulo();
+    
+    public void modificarUnidades();
+    
+    public void retirarAriculo();
+    
+    public void mostrarStock();
+    
+    public void valorFloristeria();
+    
+    public void venta();
+    
+    public void historialVenta();
+    
+    public void totalVentas();
+    
+    public void exit();
+}
