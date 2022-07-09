3aec6581a4d31ad337898fd90c6b92a21e46e9d5
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 19:42:06 +0200

    Update FloristeriaVista.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/FloristeriaVista.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/FloristeriaVista.java
@@ -3,6 +3,7 @@ package com.calvogasullmartin.t3_floristeria.vistas;
 import com.calvogasullmartin.t3_floristeria.utils.InOut;
 import com.calvogasullmartin.t3_floristeria.controladores.ArrancarAppControlador;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,16 +11,17 @@ public class FloristeriaVista {
 
     private InOut io;
 
-    public FloristeriaVista() {
-        this.io = new InOut();
+    public FloristeriaVista() {        
+        this.io = new InOut();        
     }
 
     public void interactuar(ArrancarAppControlador controlador) {
-        String nombre = pedirNombre();
+        String nombre = pedirNombre();        
         try {
             controlador.iniciarFloristeria(nombre);
             controlador.guardarUnicaFloristeria();
-            io.write("Floristeria guardada con éxito.");
+            //io.write("Floristeria guardada con éxito.");
+            System.out.println("Floristeria guardada con éxito.");
         } catch (IOException ex) {
             ////que hacer con la excepcion
         }
@@ -28,16 +30,20 @@ public class FloristeriaVista {
     private String pedirNombre() {
         String nombre = null;
         boolean ok = false;
-        int minNumChar = 3;
+        int minNumChar = 3;        
         do {
-            nombre = io.readString("Introduce el nombre de la floristeria: ");
+            //nombre = io.readString("Introduce el nombre de la floristeria: ");
+            Scanner entrada = new Scanner(System.in);
+            System.out.println("Introduce el nombre de la floristeria: ");
+            nombre = entrada.nextLine();
             if (nombre.length() < minNumChar) {
-                io.writeError("de al menos 3 caracteres");
+                //io.writeError("de al menos 3 caracteres");
+                System.out.println("Error: el nombre debe tener al menos 3 caracteres.");
             } else {
                 ok = true;
             }
         } while (!ok);
-        return null;
+        return nombre;
     }
 
 }
