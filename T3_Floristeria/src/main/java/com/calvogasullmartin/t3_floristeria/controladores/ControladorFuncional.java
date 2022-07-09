24ac1ea5d9eee372c07043a2b8c857ea28aa4598
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 16:59:13 +0200

    Update ControladorFuncional.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ControladorFuncional.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ControladorFuncional.java
@@ -1,11 +1,11 @@
 package com.calvogasullmartin.t3_floristeria.controladores;
 
-import com.calvogasullmartin.t3_floristeria.modelos.Aplicacion;
+import com.calvogasullmartin.t3_floristeria.modelos.Estado;
 
 public abstract class ControladorFuncional extends Controlador implements ControladorFuncionalInterface{
 
-    public ControladorFuncional(Aplicacion aplicacion) {
-        super(aplicacion);
+    public ControladorFuncional(Estado estado) {
+        super(estado);
     }
     
     @Override
