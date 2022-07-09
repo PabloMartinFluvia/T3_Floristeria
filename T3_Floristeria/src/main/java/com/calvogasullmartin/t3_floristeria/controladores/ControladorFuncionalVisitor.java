9123b4930fc920d72c4370006e310e84230b957d
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 19:41:32 +0200

    Update ControladorFuncionalVisitor.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ControladorFuncionalVisitor.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ControladorFuncionalVisitor.java
@@ -2,5 +2,6 @@ package com.calvogasullmartin.t3_floristeria.controladores;
 
 public interface ControladorFuncionalVisitor {
     
+    // una nueva linia
     void visitar (ArrancarAppControlador arrancarAppControlador);
 }
