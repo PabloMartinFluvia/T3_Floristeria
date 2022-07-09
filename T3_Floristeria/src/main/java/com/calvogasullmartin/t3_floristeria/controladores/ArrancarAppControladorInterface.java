5319eb650a529132f7835e7e4252de205f3ab2be
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 16:59:09 +0200

    Update ArrancarAppControladorInterface.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ArrancarAppControladorInterface.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ArrancarAppControladorInterface.java
@@ -1,18 +1,20 @@
 package com.calvogasullmartin.t3_floristeria.controladores;
 
+import java.io.IOException;
+
 public interface ArrancarAppControladorInterface extends ControladorFuncionalInterface{
     
     //los métodos específicos de un ArrancarAppControlador
     
     public boolean isPrimeraVez();
     
-    public void iniciarPersistencia();
+    public void iniciarPersistencia() throws IOException;
      
-    public void iniciarFloristeria(String nombre);
+    public void iniciarFloristeria(String nombre) throws IOException;
     
-    public void guardarUnicaFloristeria();
+    public void guardarUnicaFloristeria() throws IOException;
     
-    public String getNombreFloristeria();
+    public String getNombreFloristeria() throws IOException;
     
     public void seleccionarMenu();    
 }
