fb8404c7cdd2ab242c9cec9445c0894e9c2f1c75
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 16:32:48 +0200

    Update VistaInterface.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/VistaInterface.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/VistaInterface.java
@@ -1,9 +1,9 @@
 package com.calvogasullmartin.t3_floristeria;
 
-import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncional;
 import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncionalVisitor;
+import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncionalInterface;
 
 public interface VistaInterface extends ControladorFuncionalVisitor{
     
-    public void interactuar(ControladorFuncional controlador);
+    public void interactuar(ControladorFuncionalInterface controlador);
 }
