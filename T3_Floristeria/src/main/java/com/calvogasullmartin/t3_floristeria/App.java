3c8e3e611d6427fc20da19eebfb67b3f67c40fc3
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 19:41:26 +0200

    Update App.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/App.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/App.java
@@ -5,34 +5,43 @@ import com.calvogasullmartin.t3_floristeria.controladores.locales.Logica;
 import com.calvogasullmartin.t3_floristeria.vistas.AppVista;
 import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncional;
 
-
 public class App {
-    
+
     private LogicaInterface logica;
-    
-    private VistaInterface vistaPrincipal;        
-    
-    public App (LogicaInterface logica, VistaInterface vistaPrincipal){
-        configurar();    
+
+    private VistaInterface vistaPrincipal;
+
+    public App(LogicaInterface logica, VistaInterface vistaPrincipal) {
+        configurar();
         this.logica = logica;
         this.vistaPrincipal = vistaPrincipal;
-    }            
-    
-    private void configurar(){
-        new Configurador().configurarDaoFactory();         
     }
-    
-    private void ejecutar(){
+
+    private void configurar() {
+        new Configurador().configurarDaoFactory();
+    }
+
+    private void ejecutar() {
+        
         ControladorFuncional controlador;        
-        do{
+        do{            
             controlador = logica.getControladorFuncional();
             if (controlador != null){
                 vistaPrincipal.interactuar(controlador);
             }            
         }while(controlador != null);
-    } 
-    
-    public static void main(String[] args) {        
+         
+        /*
+        ControladorFuncional controlador;
+        controlador = logica.getControladorFuncional();
+        if (controlador != null) {
+            vistaPrincipal.interactuar(controlador);
+        }        
+        controlador = logica.getControladorFuncional();
+        */
+    }
+
+    public static void main(String[] args) {
         new App(new Logica(), new AppVista()).ejecutar();
     }
 }
