76b961d4d0618e29091b6f2e84111187ae908a81
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 11:53:25 +0200

    Create ControladorFuncionalVisitor.java

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ControladorFuncionalVisitor.java
@@ -0,0 +1,6 @@
+package com.calvogasullmartin.t3_floristeria.controladores;
+
+public interface ControladorFuncionalVisitor {
+    
+    void visitar (ArrancarAppControladorInterface arrancarAppControlador);
+}
