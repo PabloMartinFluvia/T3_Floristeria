b7b6246d751c9fdf784ceb4cc6f35f38dd113622
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 16:59:17 +0200

    Update ArrancarAppControlador.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ArrancarAppControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ArrancarAppControlador.java
@@ -2,44 +2,62 @@ package com.calvogasullmartin.t3_floristeria.controladores;
 
 import com.calvogasullmartin.t3_floristeria.modelos.Estado;
 import com.calvogasullmartin.t3_floristeria.modelos.Floristeria;
+import com.calvogasullmartin.t3_floristeria.persistencia.Conector;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author paumf
- */
-public class ArrancarAppControlador extends ControladorFuncional{
+public class ArrancarAppControlador extends ControladorFuncional implements ArrancarAppControladorInterface{        
     
     private Floristeria floristeria;
-
+    
     public ArrancarAppControlador(Estado estado, Floristeria floristeria) {
-        super(estado);
+        super(estado);      
         this.floristeria = floristeria;
+    }
+    
+    @Override
+    public boolean isPrimeraVez() {
+        Conector conector = factory.getConector();
+        boolean ok = true;
+        if(conector.isBDInicizializada()){
+            ok=false;
+        }
+        else{
+            ok = true;
+        }
+        return ok;
+    }
+
+   @Override
+    public void iniciarPersistencia() throws IOException {
+        factory.getConector().inicializarBD();
+    } 
+    
+
+    @Override
+    public void iniciarFloristeria(String nombre) {
+        floristeria.setFloristeria_id(1);//provisional
+        floristeria.setNombre_floristeria(nombre);        
     }
 
     @Override
-    public void controlar() {
-        
-    }
-    
-    public boolean isPrimeraVez(){
-        return false;
-    }
-    
-    public String getNombreFloristeria() throws IOException{
-        return factory.getFloristeriaDao().findName();
-    }
-    
-    public void setEstado(Estado estado){
-        this.estado = estado;
-    }
-    
-    public void guardarUnicaFloristeria (String nombre) throws IOException{
-        floristeria.setId(1);
-        floristeria.setNombre(nombre);
-        floristeria.setValorTotal(0);
+    public void guardarUnicaFloristeria() throws IOException {
         factory.getFloristeriaDao().create(floristeria);
     }
+
+    @Override
+    public String getNombreFloristeria() throws IOException {
+        return factory.getFloristeriaDao().findName();
+    }
+
+    @Override
+    public void seleccionarMenu() {
+        this.setEstado(Estado.EN_MENU);
+    }
     
-    
+    @Override
+    public void aceptar(ControladorFuncionalVisitor controlador) {
+        controlador.visitar(this);
+    }          
 }
