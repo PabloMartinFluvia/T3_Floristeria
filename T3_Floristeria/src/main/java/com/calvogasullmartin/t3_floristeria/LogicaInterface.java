38cd24e3cf4cd868617853df8d0e37124ec8c255
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 11:21:52 +0200

    Create LogicaInterface.java

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/LogicaInterface.java
@@ -0,0 +1,8 @@
+package com.calvogasullmartin.t3_floristeria;
+
+import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncional;
+
+public interface LogicaInterface {
+    
+    ControladorFuncional getControladorFuncional();
+}
