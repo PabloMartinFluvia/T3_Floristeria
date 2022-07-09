2c4f5184ec4e0f121c8b616b0520e99b5959ea0c
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 14:29:01 +0200

    Update BienvenidaVista.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/BienvenidaVista.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistas/BienvenidaVista.java
@@ -1,16 +1,30 @@
 package com.calvogasullmartin.t3_floristeria.vistas;
 
-import com.calvogasullmartin.t3_floristeria.controladores.ControladorFuncional;
+import com.calvogasullmartin.t3_floristeria.controladores.ArrancarAppControladorInterface;
+import com.calvogasullmartin.t3_floristeria.utils.InOut;
 
 public class BienvenidaVista {
-    
-    private ControladorFuncional controlador;
+    private InOut io;
 
-    public BienvenidaVista(ControladorFuncional controlador) {
-        this.controlador = controlador;
+    public BienvenidaVista() {
+        io = new InOut();
     }
     
-    public void darBienvenida(){
-        
+    //solo interactua con un ArrncarAppControlador cualquiera
+    public void interactuar(ArrancarAppControladorInterface controlador){
+        assert controlador != null;
+        if (controlador.isPrimeraVez()){
+            controlador.iniciarPersistencia();
+            io.writeln("Capa de persistencia inizializada con exito");
+            new FloristeriaVista().interactuar(controlador);
+            
+        }
+        String nombre = controlador.getNombreFloristeria();
+        darBienvienida(nombre);
+    }
+    
+    private void darBienvienida(String nombre){    
+        assert nombre != null;        
+        io.writeln("Bienvenid@ al gestor de la floristeria "+nombre+", desarrollado por CalvoGasullMartin ");
     }
 }
