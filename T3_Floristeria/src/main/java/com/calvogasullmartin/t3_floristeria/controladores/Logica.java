dde75c5dfad33dd498d653af6817e869bd718b5b
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-09 13:26:25 +0200

    Update Logica.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/Logica.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/Logica.java
@@ -1,45 +1,41 @@
 package com.calvogasullmartin.t3_floristeria.controladores;
 
 
+import com.calvogasullmartin.t3_floristeria.LogicaInterface;
+import com.calvogasullmartin.t3_floristeria.modelos.Aplicacion;
 import com.calvogasullmartin.t3_floristeria.modelos.Estado;
-import com.calvogasullmartin.t3_floristeria.modelos.Floristeria;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Logica {    
+public class Logica implements LogicaInterface{    
     //atributos:
     //A) las entidades del modelo (contienen info del
-    //B) els controladors 
+    //B) els controladors concrets (familia segun que familia concreta de controladores pertenezca esta Logia)
     
-    private Estado estado;
-    private Floristeria floristeria;
-    
+    private Aplicacion aplicacion; //contiene estado, floristeria, y otros modelos
+        
     // los controladores se asocian a los modelos con los que "trabajan"
     private ArrancarAppControlador arrancarAppControlador; 
-    private Map<Estado,ControladorFuncional> controladores;
-    
+    //añadir más a medida que se amplie la funcionalidad
+    private Map<Estado,ControladorFuncional> controladores;            
 
-    public Logica(Estado estado) {
-        this.estado = estado;                
-        floristeria = new Floristeria();//atributs sense inicialitzar
-        this.arrancarAppControlador = new ArrancarAppControlador(estado, floristeria);
+    public Logica() {
+        aplicacion = new Aplicacion(); // la aplicacion pone el estado = INICIAL
+        this.arrancarAppControlador = new ArrancarAppControlador(
+                aplicacion.getEstado(), aplicacion.getFloristeria());
         
         controladores = new HashMap<>();
         coordinarControladores();
     }
     
-    private void coordinarControladores(){
-        controladores.put(Estado.INITIAL, arrancarAppControlador);        
+    private void coordinarControladores(){        
+        controladores.put(Estado.INITIAL, arrancarAppControlador);  
+        //añadir más a medida que se amplie la funcionalidad
+        controladores.put(Estado.EXIT, null);
     }
-    
-    // El enum Estado es quien guarda "en que situación se encuentra la App"
-    // Segun la situación se usará un controlador X o Y o....
-    // Cuando la "tarea" a realizar se completa, entonces el controlador canvia el valor del Estado.
-    public ControladorFuncional getControlador(){
-        if (estado != Estado.EXIT){
-            return controladores.get(estado);
-        }else {
-            return null;
-        }
+        
+    @Override
+    public ControladorFuncional getControladorFuncional(){
+            return controladores.get(aplicacion.getEstado());        
     }
 }
