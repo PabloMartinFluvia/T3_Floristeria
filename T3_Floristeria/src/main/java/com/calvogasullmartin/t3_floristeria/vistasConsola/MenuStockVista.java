7b595c22ca0d2883c255be17d86e24b8a10ec6d0
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-10 10:49:17 +0200

    creado el contructor + declarar metodo interactuar con este controlador

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistasConsola/MenuStockVista.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistasConsola/MenuStockVista.java
@@ -1,5 +1,13 @@
 package com.calvogasullmartin.t3_floristeria.vistasConsola;
 
+import com.calvogasullmartin.t3_floristeria.controladores.AddProductoControlador;
+
 public class MenuStockVista {
+
+    public MenuStockVista() {
+    }        
     
+    public void interactuar(AddProductoControlador controlador){
+        
+    }
 }
