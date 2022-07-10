5d1098e07cd83315934ca189ae009dbfc0fdae6e
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-10 11:57:05 +0200

    Update AddProductoControlador.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/AddProductoControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/AddProductoControlador.java
@@ -1,5 +1,28 @@
 package com.calvogasullmartin.t3_floristeria.controladores;
 
+import com.calvogasullmartin.t3_floristeria.modelos.Altura;
+import com.calvogasullmartin.t3_floristeria.modelos.Categoria;
+import com.calvogasullmartin.t3_floristeria.modelos.Material;
+
 public interface AddProductoControlador extends ControladorPadre{
     
+    void inicializarNuevoProducto();
+    
+    void almacenarCategoria(Categoria categoria);
+    
+    void almacenarPrecio(float precio);
+    
+    void almacenarAltura(Altura altura);
+    
+    void almacenarColor(String color);
+    
+    void almacenarMaterial(Material material);
+    
+    boolean isNuevo();
+    
+    void guardarProducto();
+    
+    String getInfoProductoSinNulls();
+    
+    void seleccionarMenu();
 }
