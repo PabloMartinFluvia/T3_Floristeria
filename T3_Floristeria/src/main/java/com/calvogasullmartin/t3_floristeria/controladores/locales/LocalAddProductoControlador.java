a041d95ba992de17b6b3eba19793229c7dc834d0
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-10 10:36:08 +0200

    creado el constructor

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/locales/LocalAddProductoControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/locales/LocalAddProductoControlador.java
@@ -2,9 +2,14 @@ package com.calvogasullmartin.t3_floristeria.controladores.locales;
 
 import com.calvogasullmartin.t3_floristeria.controladores.AddProductoControlador;
 import com.calvogasullmartin.t3_floristeria.controladores.ControladorPadreVisitor;
+import com.calvogasullmartin.t3_floristeria.modelos.Estados;
 
 public class LocalAddProductoControlador extends LocalControladorPadre implements AddProductoControlador{
 
+    public LocalAddProductoControlador(Estados estados) {
+        super(estados);
+    }
+    
     @Override
     public void aceptar(ControladorPadreVisitor controlador) {
         controlador.visitar(this);
