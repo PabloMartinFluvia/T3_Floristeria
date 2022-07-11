6445ff212bef54d10d0284f66286e601d73347cc
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-11 14:14:42 +0200

    Create ModificarProductoControlador.java

---
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/ModificarProductoControlador.java
@@ -0,0 +1,8 @@
+package com.calvogasullmartin.t3_floristeria.controladores;
+
+public interface ModificarProductoControlador extends MostrarConjuntoControlador{
+    
+    //ojo, pk hereda de MostrarConjuntoControlador (para heredar sus metodos). Dará problemas en accept / visit?
+    
+     public boolean IsUpdate();
+}
