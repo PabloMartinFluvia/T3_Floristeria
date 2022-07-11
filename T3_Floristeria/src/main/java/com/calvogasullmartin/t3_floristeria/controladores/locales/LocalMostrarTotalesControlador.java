5845481cbce6315512b490a85718d32f233cb5ce
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-11 13:12:08 +0200

    Update LocalMostrarTotalesControlador.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/locales/LocalMostrarTotalesControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/locales/LocalMostrarTotalesControlador.java
@@ -2,17 +2,42 @@ package com.calvogasullmartin.t3_floristeria.controladores.locales;
 
 import com.calvogasullmartin.t3_floristeria.controladores.ControladorPadreVisitor;
 import com.calvogasullmartin.t3_floristeria.controladores.MostrarTotalesControlador;
+import com.calvogasullmartin.t3_floristeria.modelos.Estado;
 import com.calvogasullmartin.t3_floristeria.modelos.Estados;
+import java.io.IOException;
 
 public class LocalMostrarTotalesControlador extends LocalControladorPadre implements MostrarTotalesControlador{
 
-    public LocalMostrarTotalesControlador(Estados estados) {
+    private boolean mostrarValorTienda; // si false -> muestra total Facturacion
+    
+    public LocalMostrarTotalesControlador(Estados estados, boolean mostrarValorTienda) {
         super(estados);
+        this.mostrarValorTienda = mostrarValorTienda;
     }
     
     @Override
     public void aceptar(ControladorPadreVisitor controlador) {
         controlador.visitar(this);
+    }
+
+    @Override
+    public boolean isMostrarValorTienda() {
+        return mostrarValorTienda;
+    }
+
+    @Override
+    public void seleccionarMenu() {
+        this.setEstado(Estado.EN_MENU);
+    }
+
+    @Override
+    public float getTotalStocks() throws IOException {
+        return factory.getFloristeriaDao().findTotalStocks();
+    }
+
+    @Override
+    public float getTotalTiquets() throws IOException {
+        return factory.getFloristeriaDao().findTotalTiquets();
     }
     
 }
