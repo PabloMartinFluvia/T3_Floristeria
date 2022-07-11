ccc97d935e69a832e04ba6b63bddd78f5c8e6098
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-11 13:07:48 +0200

    Update MostrarTotalesControlador.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/MostrarTotalesControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/MostrarTotalesControlador.java
@@ -1,5 +1,14 @@
 package com.calvogasullmartin.t3_floristeria.controladores;
 
+import java.io.IOException;
+
 public interface MostrarTotalesControlador extends ControladorPadre{
+
+    public boolean isMostrarValorTienda();
     
+    public void seleccionarMenu();
+    
+    public float getTotalStocks() throws IOException;
+    
+    public float getTotalTiquets() throws IOException;
 }
