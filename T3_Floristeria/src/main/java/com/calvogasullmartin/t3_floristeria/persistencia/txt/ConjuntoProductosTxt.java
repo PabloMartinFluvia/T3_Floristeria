d03c9dc1b440e1ea227c0ad4bc2b1d83a0e25342
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-11 11:22:26 +0200

    Update ConjuntoProductosTxt.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/persistencia/txt/ConjuntoProductosTxt.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/persistencia/txt/ConjuntoProductosTxt.java
@@ -3,12 +3,42 @@ package com.calvogasullmartin.t3_floristeria.persistencia.txt;
 import com.calvogasullmartin.t3_floristeria.modelos.ConjuntoProductos;
 import com.calvogasullmartin.t3_floristeria.persistencia.ConjuntoProductosDao;
 import java.io.IOException;
+import java.util.List;
 
 public class ConjuntoProductosTxt extends GenericDaoTxt<ConjuntoProductos, Integer> implements ConjuntoProductosDao{
 
     @Override
     public void addToConjuntoValue(int idConjunto, float increment) throws IOException {
-        // augmentar / reducir el valor del conjunto (que tenga ese id) segun esta cantidad
+        /*
+        augmentar / reducir el valor del conjunto (que tenga ese id) segun esta cantidad
+        */        
+    }
+
+    @Override
+    public List<ConjuntoProductos> findAllStocks() throws IOException {
+        List<ConjuntoProductos> stocks = null;
+        /*
+        pendiente, devolver todos los stocks
+        */
+        return stocks;
+    }
+
+    @Override
+    public List<ConjuntoProductos> findAllTiquets() throws IOException {
+        List<ConjuntoProductos> tiquets = null;
+        /*
+        pendiente, devolver todos los tiquets
+        */
+        return tiquets;
+    }
+
+    @Override
+    public ConjuntoProductos findOneStockById(Integer id) throws IOException {
+        ConjuntoProductos stock = null;
+        /*
+        pendiente, devolver el stock que tenga ese id
+        */
+        return stock;
     }
 
 }
