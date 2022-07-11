4116f1d306d6d098e50acda24eb44be45f9b7616
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-11 13:07:27 +0200

    Update FloristeriaVista.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistasConsola/FloristeriaVista.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/vistasConsola/FloristeriaVista.java
@@ -2,20 +2,22 @@ package com.calvogasullmartin.t3_floristeria.vistasConsola;
 
 import com.calvogasullmartin.t3_floristeria.utils.InOut;
 import com.calvogasullmartin.t3_floristeria.controladores.ArrancarAppControlador;
+import com.calvogasullmartin.t3_floristeria.controladores.MostrarTotalesControlador;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class FloristeriaVista {
 
     private InOut io;
+    
+    private MostrarTotalesControlador controladorTotales;
 
     public FloristeriaVista() {        
         this.io = new InOut();        
     }
 
     public void interactuar(ArrancarAppControlador controlador) {
+        assert controlador != null;
         String nombre = pedirNombre();        
         try {
             controlador.iniciarFloristeria(nombre);
@@ -23,8 +25,43 @@ public class FloristeriaVista {
             //io.write("Floristeria guardada con éxito.");
             System.out.println("Floristeria guardada con éxito.");
         } catch (IOException ex) {
-            ////que hacer con la excepcion
+            /*
+            mensaje de que no se ha podido consultar a la BD
+            */
         }
+    }
+    
+    public void interactuar(MostrarTotalesControlador controlador){
+        assert controlador != null;
+        this.controladorTotales = controlador;
+        try {
+            if (controladorTotales.isMostrarValorTienda()){
+                mostrarTotalStocks();            
+            }else {
+                mostrarTotalVentas();            
+            }
+        } catch (IOException ex){
+            /*
+            mensaje de que no se ha podido consultar a la BD
+            */
+        }
+        controladorTotales.seleccionarMenu();
+    }
+    
+    private void mostrarTotalStocks () throws IOException{
+        float total = controladorTotales.getTotalStocks();
+        imprimirTotal("todos los stocks", total);
+    }
+    
+    private void mostrarTotalVentas() throws IOException{
+        float total = controladorTotales.getTotalTiquets();
+        imprimirTotal("todas las ventas", total);
+    }
+    
+    private void imprimirTotal(String title, float total){
+        String mensage = "El importe total de "+title+" es de: "+total;
+        //io.writeln(mensage);
+        System.out.println(mensage);
     }
 
     private String pedirNombre() {
