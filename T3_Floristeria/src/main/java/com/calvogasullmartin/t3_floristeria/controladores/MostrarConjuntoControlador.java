72a3d94ffcad14477259753dd11a4045d2deff91
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-11 10:02:37 +0200

    Update MostrarConjuntoControlador.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/MostrarConjuntoControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/MostrarConjuntoControlador.java
@@ -1,5 +1,19 @@
 package com.calvogasullmartin.t3_floristeria.controladores;
 
+import java.io.IOException;
+import java.util.List;
+
 public interface MostrarConjuntoControlador extends ControladorPadre{
     
+    public boolean isIsStock();
+    
+    public void setWithUnits(boolean withUnits);
+    
+    public boolean isWithUnits(); 
+    
+    public List<List<String>> getAllConjuntos() throws IOException;
+    
+    public List<String> getOneConjuntos(int conjundo_id) throws IOException;
+    
+    public void setInMenu();
 }
