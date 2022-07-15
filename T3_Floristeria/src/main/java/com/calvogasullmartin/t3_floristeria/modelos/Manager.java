8b1d4e757b07ced468c097362d643b2728fa4962
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-15 10:12:27 +0200

    Update Manager.java

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/modelos/Manager.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/modelos/Manager.java
@@ -4,7 +4,9 @@ public class Manager {
     
     private Estado estado;
     
-    private final int NUM_CATEGORIAS = 3; //3 tipos de productos = 3 stocks distintos    
+    private final int NUM_CATEGORIAS = 3; //3 tipos de productos = 3 stocks distintos  
+    
+    final String errorBD = "No se ha podido acceder a la base de datos";
 
     public Manager() {
         this.estado = Estado.INITIAL; // cuando se crea el manager se pone el estado inicial
@@ -21,5 +23,8 @@ public class Manager {
     public int getNUM_CATEGORIAS() {
         return NUM_CATEGORIAS;
     }
-            
+
+    public String getErrorBD() {
+        return errorBD;
+    }                    
 }
