4940f5134cdc46aa693a688f37272317a7338766
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-07-18 19:18:08 +0200

    update

--- T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/locales/LocalControlador.java
+++ T3_Floristeria/src/main/java/com/calvogasullmartin/t3_floristeria/controladores/locales/LocalControlador.java
@@ -2,13 +2,17 @@ package com.calvogasullmartin.t3_floristeria.controladores.locales;
 
 import com.calvogasullmartin.t3_floristeria.modelos.Estado;
 import com.calvogasullmartin.t3_floristeria.modelos.Manager;
+import com.calvogasullmartin.t3_floristeria.persistencia.DaoFactory;
 
 public abstract class LocalControlador {
     
     private Manager manager;
+    
+    protected final DaoFactory factory;
 
     public LocalControlador(Manager manager) {
         this.manager = manager;
+        this. factory = DaoFactory.getFactory();
     }
                 
     public void setEstado(Estado estado){
